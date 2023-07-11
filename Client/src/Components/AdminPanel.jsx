d630250a80b2712e96fd4906e060d47f31b60e90
noumaan <noumaanahamed2@gmail.com>
2023-07-11 07:42:20 +0530

    added update functionalty

--- Client/src/Components/AdminPanel.jsx
+++ Client/src/Components/AdminPanel.jsx
@@ -15,6 +15,7 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Link from "@mui/material/Link";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { TextField } from "@mui/material";
 
 function Copyright() {
   return (
@@ -29,13 +30,20 @@ function Copyright() {
   );
 }
 
-const cards = [1, 2, 3];
-
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
 export default function Album() {
-  React.useEffect(() => {
+  const [cards, setCards] = React.useState([]);
+  const [selectedCard, setSelectedCard] = React.useState(null);
+  const [editFormData, setEditFormData] = React.useState({
+    title: "",
+    description: "",
+    imageLink: "",
+    price: 0,
+  });
+
+  const fetchCourses = () => {
     fetch("http://localhost:3000/admin/courses", {
       method: "GET",
       headers: {
@@ -43,8 +51,78 @@ export default function Album() {
       },
     })
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        // console.log(cards);
+        setCards(data.Courses);
+      });
+  };
+
+  React.useEffect(() => {
+    fetchCourses();
+
+    const interval = setInterval(fetchCourses, 5000); // Fetch every 5 seconds
+
+    return () => clearInterval(interval); // Clean up the interval on component unmount
   }, []);
+
+  function viewCourse(id) {
+    console.log(id);
+  }
+
+  function editCourse(card) {
+    setSelectedCard(card);
+    setEditFormData({
+      title: card.title,
+      description: card.description,
+      imageLink: card.imageLink,
+      price: parseInt(card.price),
+    });
+  }
+
+  function handleFormChange(e) {
+    setEditFormData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  function handleFormSubmit() {
+    fetch(`http://localhost:3000/admin/courses/${selectedCard.id}`, {
+      method: "PUT",
+      headers: {
+        "COntent-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: JSON.stringify({ ...editFormData }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.message);
+        console.log(cards);
+
+        setSelectedCard(null);
+        setEditFormData({
+          title: "",
+          description: "",
+          imageLink: "",
+          price: 0,
+        });
+      });
+  }
+
+  function closeUpdate() {
+    setSelectedCard(null);
+    setEditFormData({
+      title: "",
+      description: "",
+      imageLink: "",
+      price: 0,
+    });
+  }
+
+  function deleteCourse(id) {
+    console.log(id);
+  }
 
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -88,7 +166,61 @@ export default function Album() {
         </Box>
         <Container sx={{ py: 8 }} maxWidth="md">
           {/* End hero unit */}
+          {selectedCard && (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                margin: "0 2rem 2rem 2rem",
+              }}
+            >
+              <TextField
+                id="outlined-basic"
+                label="Title"
+                variant="outlined"
+                value={editFormData.title}
+                name="title"
+                placeholder="title"
+                onChange={handleFormChange}
+              />
+              <TextField
+                id="outlined-basic"
+                label="Description"
+                variant="outlined"
+                value={editFormData.description}
+                name="description"
+                placeholder="description"
+                onChange={handleFormChange}
+              />
+
+              <TextField
+                id="outlined-basic"
+                label="ImageLink"
+                variant="outlined"
+                value={editFormData.imageLink}
+                name="imageLink"
+                placeholder="image"
+                onChange={handleFormChange}
+              />
+              <TextField
+                id="outlined-basic"
+                label="Price"
+                variant="outlined"
+                value={editFormData.price}
+                name="price"
+                placeholder="price"
+                onChange={handleFormChange}
+              />
+              <Button onClick={closeUpdate} variant="contained" color="error">
+                Close
+              </Button>
+              <Button onClick={handleFormSubmit} variant="contained">
+                Update
+              </Button>
+            </div>
+          )}
           <Grid container spacing={4}>
+            {console.log(cards)}
             {cards.map((card) => (
               <Grid item key={card} xs={12} sm={6} md={4}>
                 <Card
@@ -104,21 +236,25 @@ export default function Album() {
                       // 16:9
                       pt: "56.25%",
                     }}
-                    image="https://source.unsplash.com/random?wallpapers"
+                    image={card.imageLink}
                   />
                   <CardContent sx={{ flexGrow: 1 }}>
                     <Typography gutterBottom variant="h5" component="h2">
-                      Heading
+                      {card.title}
                     </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe
-                      the content.
-                    </Typography>
+                    <Typography>{card.description}</Typography>
+                    <Typography textAlign={"right"}>₹{card.price}</Typography>
                   </CardContent>
                   <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
-                    <Button size="small">Delete</Button>
+                    <Button onClick={() => viewCourse(card.id)} size="small">
+                      View
+                    </Button>
+                    <Button onClick={() => editCourse(card)} size="small">
+                      Edit
+                    </Button>
+                    <Button onClick={() => deleteCourse(card.id)} size="small">
+                      Delete
+                    </Button>
                   </CardActions>
                 </Card>
               </Grid>
